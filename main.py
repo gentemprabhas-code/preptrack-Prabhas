13516064ac7f014df9dc28bd89fbb31b4e2317c9
prabas <gentemprabhas@gmail.com>
2026-08-06 19:44:44 +0530

    student name validation

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ print("=" * 50)
 print("=" * 50)
 print("              COLLECT STUDENT DETAILS")
 print("=" * 50)
+#student_name validation
+while True:
+    student_name = input("Enter student name: ")
+
+    if student_name.strip() != "":
+        break
+
+    print("Student name cannot be empty.")
+    print()
+
+print()
