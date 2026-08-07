aea013942872b637debf69c46c652ba24b1804eb
prabas <gentemprabhas@gmail.com>
2026-08-07 09:03:02 +0530

    Initialize

--- main.py
+++ main.py
@@ -42,3 +42,40 @@ while True:
         break
     else:
         print("Invalid input. Enter yes or no.")
+#Profile-Verification Input
+while True:
+    profile_input = input("Is the student profile verified? (yes/no): ").lower()
+    if profile_input in ["yes", "no"]:
+        break
+    else:
+        print("Invalid input. Enter yes or no.")
+# --------------------------------------------------
+# 2. INITIALIZE COUNTERS AND ACCUMULATORS
+# --------------------------------------------------
+print("=" * 50)
+print("              INITIALIZE COUNTERS AND ACCUMULATORS")
+print("=" * 50)
+total_score = 0
+
+attempted_days = 0
+absent_days = 0
+passed_days = 0
+failed_days = 0
+
+strong_days = 0
+satisfactory_days = 0
+improvement_days = 0
+critical_days = 0
+
+highest_score = 0
+highest_score_day = 0
+
+lowest_score = 0
+lowest_score_day = 0
+
+first_attempt_found = False
+
+critical_score_found = False
+first_critical_day = 0
+first_critical_score = 0
+
